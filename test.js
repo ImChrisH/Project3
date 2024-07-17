8e42742ecd1df671c08f1821342616b806015eab
ImChrisH <hui.simplicity@gmail.com>
2024-07-17 13:08:40 -0400

    Add files via upload

---
+++ test.js
@@ -0,0 +1,71 @@
+console.log("hi");
+
+let counter=10;
+let highscore=0;
+let num=Math.trunc(Math.random()*100)+1;
+let msg=document.querySelector('.msg');
+let guess=document.querySelector('.guess');
+let gBtn=document.querySelector('.gBtn');
+let currentcounter=document.querySelector('.counter');
+let imageshown= "gameover.jpg";
+let hScore=document.querySelector('.hScore');
+
+
+// Previous guess testing
+let prevGuesses=document.querySelector('.prevGuessA');
+let prevGuessArray=[];
+
+gBtn.addEventListener('click', guessing);
+
+function guessing(){
+        if(guess.value>num){
+            msg.textContent= "Wrong! Too High!";
+            guess.value=''
+            counter=counter-1;
+            counter.textContent=String(counter);
+        
+    }
+        else if (guess.value < num){
+            msg.textContent= "Wrong! Too Low!";
+            msg.style.color='red';
+            msg.style.fontWeight='bold'; 
+            prevGuessArray.push(guess.value);
+            counter=counter-1;
+            currentcounter.textContent=String(counter);
+            
+        
+        }
+
+        else if (guess.value==num){
+            msg.textContent= "Congratulations, you guessed correctly!";
+            gBtn.disabled = true;
+            highscore=highscore+1;
+            hScore.textContent=String(highscore);
+
+            prevGuessArray.push(guess.value);
+            guess.value='';
+        }
+
+        else if(guess.value>100){
+            msg.textContent= "Only numbers from 1 to 100!";
+        }
+
+        else if(guess.value <1){
+            msg.textContent= "Only numbers from 1 to 100!";
+        }
+
+        if(counter<=0){
+            msg.textContent="GAME OVER";
+            msg.style.fontSize="x-large";
+            gBtn.disabled = true;
+        }
+            
+       
+
+
+
+}
+
+console.log(num);
+
+
